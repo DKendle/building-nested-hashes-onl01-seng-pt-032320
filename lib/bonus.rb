d1be6997b80caccd163106200aac87669a5b3bda
Dina Kendle <dina.kendle@gmail.com>
2020-04-04 19:59:18 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -1,7 +1,4 @@
 def bonus
-  epic_tragedy[:montague][:hero][:status] = "dead"
-  epic_tragedy[:capulet][:heroine][:status] = "dead"
-  
   
   epic_tragedy = {
    :montague => {
@@ -22,10 +19,13 @@ def bonus
           {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
+
   }
 
   #code your solution here:
 
+     epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
